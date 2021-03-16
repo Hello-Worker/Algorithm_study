6d9c3cbceb1c539af665b93001a915a991e40f92
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2021-03-16 18:42:05 +0900

    Add files via upload

--- 알고리즘1-49단계/4단계_while문/1110.py
+++ 알고리즘1-49단계/4단계_while문/1110.py
@@ -1,9 +1,15 @@
 # 4-3) 1110번: 더하기 사이클
+
+num = int(input())
+answer = num #초기 입력받은 값을 answer에 저장.
+cnt = 0
+
 while True:
-    try:
-        N = input()
-        if N<10:
-            N.append()
-    except:
-        break;
-    print(len(N)) #len은 str을 받음.
+    ten = num//10 # 목의 기호. 10의자리수. 68에서 6
+    one = num%10 # 나머지 기호. 68을 입력받았을 시 나머지인 8(오른쪽 자리 수)
+    num = ((ten + one)%10)+one*10 # (6+8)%10+8*10 = 84
+    cnt += 1 # 사이클 횟수
+    if num == answer: # num = 초기값
+        break
+print(cnt)
+
