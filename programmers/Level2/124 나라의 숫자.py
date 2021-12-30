f75e271856f7962dccfb6ffc5791234cd17acde9
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2021-12-30 23:47:52 +0900

    Add files via upload

---
+++ programmers/Level2/124 나라의 숫자.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 2
+    cnt = 1
+    if n < 3:
+        answer += str(n)
+    
+    else:
+        for i in range(1,n+1):
+            
+
+            
+            
+        
+    return answer
