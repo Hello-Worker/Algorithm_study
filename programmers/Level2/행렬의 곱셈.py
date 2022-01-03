6f924af8af6db906f9f5b029ee4ebbe3505a9eb3
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-01-03 19:47:08 +0900

    Update 행렬의 곱셈.py

--- programmers/Level2/행렬의 곱셈.py
+++ programmers/Level2/행렬의 곱셈.py
@@ -1,6 +1,5 @@
 def solution(arr1, arr2):
     answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
-    
     for i in range(len(arr1)): 
         lists = []
         for j in range(len(arr2[0])): 
