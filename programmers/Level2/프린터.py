2dcc6b2ff6f15a95a37c8750a956b8ea6675da5a
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-01-11 00:27:11 +0900

    upload

--- programmers/Level2/프린터.py
+++ programmers/Level2/프린터.py
@@ -1,11 +1,25 @@
+# def solution(priorities, location):
+#     answer = 0
+#     max_num = max(priorities)
+#     while True:
+#         for i in range(len(priorities)):
+#             if max_num == priorities[i]:
+#                 answer += 1
+#                 priorities[i] = 0
+#                 if i == location:
+#                     return answer
+#             max_num = max(priorities)
+
 def solution(priorities, location):
+    queue = [(i,p) for i,p in enumerate(priorities)]
     answer = 0
-    max_num = max(priorities)
     while True:
-        for i in range(len(priorities)):
-            if max_num == priorities[i]:
-                answer += 1
-                priorities[i] = 0
-                if i == location:
-                    return answer
-            max_num = max(priorities)
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+solution([1, 1, 9, 1, 1, 1],2)
