545470a3c6c3b90da0bbe46c1fb0aec7825f76bb
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-02-04 02:18:34 +0900

    Upload

---
+++ programmers/Level2/기능개발.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    answer = []
+    
+    while progresses:
+        cnt = 0
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+            
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+            
+        if cnt > 0:
+            answer.append(cnt)
+        
+    return answer
