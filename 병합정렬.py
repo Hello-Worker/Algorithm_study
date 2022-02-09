131e6553f8bff330d938b83cf8e2212af055865e
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-02-10 06:15:14 +0900

    Upload

---
+++ 병합정렬.py
@@ -0,0 +1,20 @@
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr       
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    merged_arr = []
+    l = h = 0
+    while l < len(left) and h < len(right):
+        if left[l] < right[h]:
+            merged_arr.append(left[l])
+            l += 1
+        else:
+            merged_arr.append(right[h])
+            h += 1
+    merged_arr += left[l:]
+    merged_arr += right[h:]
+    return merged_arr
+
+merge_sort([6, 5, 3, 1, 8, 7, 2, 4])
