57e4d4bad7eaa7852d36d6e232dc102300880f09
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-02-18 15:18:16 +0900

    문제풀이

---
+++ Java/Baekjoon/1-50단계 문제풀이/1단계_입출력과 사칙연산/11. 1998년생인 내가 태국에서는 2541년생.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = 2541-1998;
+		
+		System.out.println(A-B);
+	}
+}
