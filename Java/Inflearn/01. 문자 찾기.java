2cf58216aacfd26dd4ed5e82f27a8b5b5cd585f5
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-05-16 17:26:06 +0900

    알고리즘 풀이

---
+++ Java/Inflearn/01. 문자 찾기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+    public int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        
+
+        return answer;
+
+    }
+	
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.print(T.solution(str, c));
+
+    }
+	
+
+}
+
+
