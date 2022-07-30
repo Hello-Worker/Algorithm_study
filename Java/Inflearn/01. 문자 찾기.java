51c22ba21e9f5b194faaa154f9750e054eab305f
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2022-07-30 23:58:45 +0900

    Study

--- Java/Inflearn/01. 문자 찾기.java
+++ Java/Inflearn/01. 문자 찾기.java
@@ -1,32 +1,22 @@
 import java.util.*;
-
-class Main {
-    public int solution(String str, char t){
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-        /*for(int i=0; i<str.length(); i++) {
-        	if(str.charAt(i)==t) answer++;
-        }*/
-        
-        for(char x :str.toCharArray()) {
-        	if (x==t) answer++;
-        }
-
-        return answer;
-
-    }
-	
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        char c = sc.next().charAt(0);
-        System.out.print(T.solution(str, c));
-
-    }
-	
-
+class Main{
+	public int solution(String str, char c) {
+		int answer=0;
+		str=str.toUpperCase();
+		c=Character.toUpperCase(c);
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i)==c) 
+				answer++;
+		}
+		return answer;
+		
+	}
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);//System.in byte로 읽는다.
+		String str=sc.next();//공백을 입력하기 전까지 값을 입력받는다.
+		char c=sc.next().charAt(0);
+		System.out.println(T.solution(str, c));
+		
+	}
 }
-
-
