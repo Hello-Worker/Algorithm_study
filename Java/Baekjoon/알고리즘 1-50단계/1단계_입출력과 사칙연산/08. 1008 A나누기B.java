6018b21b6a7948127ce0c84e7759ae628314b576
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2023-09-06 19:35:27 +0900

    코드정리

---
+++ Java/Baekjoon/알고리즘 1-50단계/1단계_입출력과 사칙연산/08. 1008 A나누기B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double A = scan.nextDouble();
+		double B = scan.nextDouble();
+		
+		System.out.println(A/B);
+		
+	}
+
+}
